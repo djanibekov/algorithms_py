8cecdac973f28d6f09d61cf29913961315844a76
Amir <amir.akilbekovich@gmail.com>
2020-08-06 16:19:10 +0500

    week 1

---
+++ karatsuba.py
@@ -0,0 +1,22 @@
+def karatsuba(x, y):
+	size = len(str(x))
+	if(size == 1):
+		return x*y
+
+	half = size // 2 
+	a = x // 10 ** half
+	b = x % 10 ** half
+	c = y // 10 ** half
+	d = y % 10 ** half
+
+	ac = karatsuba(a, c)
+	bd = karatsuba(b, d)
+	ad_bc = karatsuba(a+b, c+d) - ac - bd
+
+	return ac * 10 ** size + bd + ad_bc * 10 **half
+
+
+x = 3141592653589793238462643383279502884197169399375105820974944592
+y = 2718281828459045235360287471352662497757247093699959574966967627
+# print(type(x))
+print(karatsuba(x, y))
---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+def merge(first, second, size):
+    i = 0 
+    j = 0
+    merged = []
+    #? combining two lists
+    while i < len(first) and j < len(second):
+        if(first[i] < second[j]):
+            merged.append(first[i])
+            i = i + 1
+        else:
+            merged.append(second[j])
+            j = j + 1
+
+    #! extra check whether element is left
+    while i < len(first):
+        merged.append(first[i])
+        i = i + 1
+    while j < len(second):
+        merged.append(second[j])
+        j = j + 1
+
+    return merged
+
+def merge_sort(array):
+    length = len(array)
+    if(length == 1):
+        return array
+    mid = length // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge(left, right, length)
+
+
+print(merge_sort([1, 3, 5, 2, 4, 6]))
+
+    
